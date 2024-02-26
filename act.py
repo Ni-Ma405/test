1551b3ddcb1abf096312131a52730595de539f51
unknown <02230099.cst@rub.edu.bt>
2024-02-26 15:11:38 -0800

    act.1

---
+++ act.py
@@ -0,0 +1,6 @@
+age = int(input ("Enter your age"))
+std = input ("are you a student?(yes/no)")
+if (age <= 12 or (age >= 13 and age <= 18) and std =="yes"):
+    print("you are elligible for a discount on the movie ticket")
+else:
+    print("you are elligible for a discount on the movie ticket")
